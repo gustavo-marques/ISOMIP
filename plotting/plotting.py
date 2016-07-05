11df0e63f9fbca27f1285b50aeeda969ce6439dd
Gustavo Marques <gmarques@princeton.edu>
2016-07-05 18:42:43 -0400

    Updated smoothing script

--- plotting/plotting.py
+++ plotting/plotting.py
@@ -10,14 +10,14 @@ import subprocess
 import numpy as np
 
 # Define a function to plot a section
-def plot_section(file_handle, record, xq, j=0, variable='salt',eta='e',clim=(33.8,34.55), plot_grid=True, rep='linear', xlim=(320,800), ylim=(-720,0), cmap=plt.cm.jet):
+def plot_section(file_handle, record, xq, i=0, variable='salt',eta='e',clim=(33.8,34.55), plot_grid=True, rep='linear', xlim=(320,800), ylim=(-720,0), cmap=plt.cm.jet):
     """Plots a section of by reading vertical grid and scalar variable and super-sampling
     both in order to plot vertical and horizontal reconstructions.
     
     Optional arguments have defaults for plotting salinity and overlaying the grid.
     """
-    e = file_handle.variables[eta][record,:,j,:] # Vertical grid positions
-    s = file_handle.variables[variable][record,:,j,:] # Scalar field to color
+    e = file_handle.variables[eta][record,:,:,i] # Vertical grid positions
+    s = file_handle.variables[variable][record,:,:,i] # Scalar field to color
     x,z,q = m6toolbox.section2quadmesh(xq, e, s, representation=rep) # This yields three areas at twice the model resolution
     plt.pcolormesh(x, z, q, cmap=cmap);
     plt.colorbar()
@@ -37,23 +37,27 @@ def plot_diags(path,n1,n2,n3,n4):
 
 def plot_stats(file_handle,variable='u',labels=['layer','rho','sigma','z']):
     # Two subplots, the axes array is 1-d
-    f, ax = plt.subplots(2, sharex=True, figsize=(18,9))
-    ax[0].set_title('Min.')
-    ax[1].set_title('Max.')
+    plt.figure(figsize=(18,9))
+    plt.title('Max. absolute vel.')
     time = file_handle[0].variables['Time'][:]
     units = file_handle[0].variables['u'].units
     for i in range(len(labels)):
         smin=np.zeros(len(time)); smax=np.zeros(len(time))
+        total=np.zeros(len(time))
         s = file_handle[i].variables[variable][:]      
         for t in range(len(time)):
             smin[t]=s[t,:].min(); smax[t]=s[t,:].max()
+            if smax[t]>=np.abs(smin[t]):
+               total[t]=smax[t]
+               plt.semilogy(time[t],total[t],'+',ms=2)
+            else:
+               total[t]=np.abs(smin[t])
+               plt.semilogy(time[t],total[t],'o',ms=2)
 
-        ax[0].plot(time,smin,lw=2.0, label=labels[i])
-        ax[0].legend(loc=0, shadow=True)
-        ax[1].plot(time,smax,lw=1.5)
-        ax[1].set_xlabel('Time') 
-        ax[1].set_ylabel(units) 
-        ax[0].set_ylabel(units) 
+        plt.plot(time,total,lw=2.0, label=labels[i])
+        plt.legend(loc=0, shadow=True)
+        plt.xlabel('Time') 
+        plt.ylabel(units) 
 
 def get_rho(S,T,rho0=1027.51,Sref=34.2,Tref=-1.0,alpha = 3.733e-5, beta = 7.843e-4):
     """ Compute linear EoS """

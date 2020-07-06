7d506d6b6ae9c93acb02099dba92ef6f0cccad27
Gustavo Marques <gmarques@ucar.edu>
2020-07-06 10:31:16 -0600

    Update gamma curve

--- Docs/Reports/sigma_zstar/COM/python/meltVSgamma_COM.py
+++ Docs/Reports/sigma_zstar/COM/python/meltVSgamma_COM.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-gamma_h10 = [0.01, 0.03, 0.05, 0.07,0.09,0.11,0.13, 0.15, 0.17, 0.19]
-melt_h10 = [3.64, 12.65, 20.36, 26.26, 30.51, 33.46, 35.69, 37.06, 38.03, 38.80]
+gamma_h10 = [0.01, 0.02, 0.03, 0.04, 0.05, 0.07,0.09,0.1, 0.12]
+melt_h10 = [4.6, 12.27, 20.0, 27.3, 33.19, 41.3, 46.0, 47.4, 49.4]
 
 #coeffs = np.polyfit(gamma_com,melt_com,3) # third-order poly
 coeffs = np.polyfit(gamma_h10,melt_h10,4) # third-order poly
-new_gamma = np.arange(0.01,0.19,0.0001)
+new_gamma = np.arange(0.01,0.12,0.0001)
 new_melt = coeffs[0]*new_gamma**4 + coeffs[1]*new_gamma**3 + coeffs[2]*new_gamma**2 + coeffs[3]*new_gamma + coeffs[4]
 # find values that yields melt=30
 tmp = np.nonzero(new_melt<=30.)[-1][-1]
@@ -21,7 +21,7 @@ plt.xlabel(r'$\Gamma_T$')
 plt.ylabel('mean melt rate (m a$^{-1}$)')
 plt.xlim(0,0.2001)
 plt.savefig('melt_gamma_COM.png')
-
+plt.show()
 #plt.figure()
 #plt.plot(gamma_com,melt_com,'k-o',label='COM',lw=1)
 #plt.plot(gamma_tpy,melt_tpy,'r-o',label='TPY',lw=1)
